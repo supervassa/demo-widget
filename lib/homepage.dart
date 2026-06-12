3da64960a661f68d9133af5e86002129396caa2d
supervassa <vjivandro77@gmail.com>
2026-06-12 14:27:48 +0700

    update

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,87 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/widget_button.dart';
+import 'package:flutter_application_1/widgets/widget_textfield.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController paswordController = TextEditingController();
+  bool passenable = true;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Halaman Login'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            WidgetTextfield(
+              hintText: 'Masukkan Username Anda',
+              textEditingController: usernameController,
+              keyboardType: TextInputType.text,
+            ),
+
+            SizedBox(height: 8),
+            WidgetTextfield(
+              hintText: 'Masukkan Password Anda',
+              textEditingController: paswordController,
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: passenable,
+              suffixIcon: IconButton(
+                onPressed: () {
+                  setState(() {
+                    passenable = !passenable;
+                  });
+                },
+                icon: Icon(
+                  passenable ? Icons.visibility : Icons.visibility_off,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 8),
+            WidgetButton(
+              namabutton: 'Masuk',
+              onPressed: () {
+                // MaterialPageRoute route = MaterialPageRoute(
+                //   builder: (context) => DashboardPage(),
+                // );
+                // Navigator.push(context, route);
+                // setState(() {
+                //   usernameController.text;
+                //   paswordController.text;
+                // });
+
+                _checValue(usernameController.text, paswordController.text);
+              },
+            ),
+
+            SizedBox(height: 8),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Username: ${usernameController.text},\nPassword: ${paswordController.text}',
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _checValue(String username, String password) {
+    if (username.length == 9 && password.length >= 8) {
+      SnackBar snackBar = SnackBar(content: Text('Username $username - Password $password sudah benar'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else {
+      const snackBar = SnackBar(content: Text('Pastikan username dan password sudah benar'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/dashboard.dart';
-import 'package:flutter_application_1/widgets/widget_button.dart';
-import 'package:flutter_application_1/widgets/widget_textfield.dart';
+import 'package:flutter_application_1/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,76 +32,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController usernameController = TextEditingController();
-  final TextEditingController paswordController = TextEditingController();
-  bool passenable = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            WidgetTextfield(
-              hintText: 'Masukkan Username Anda',
-              textEditingController: usernameController,
-              keyboardType: TextInputType.text,
-            ),
-
-            SizedBox(height: 8),
-            WidgetTextfield(
-              hintText: 'Masukkan Password Anda',
-              textEditingController: paswordController,
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: passenable,
-              suffixIcon: IconButton(
-                onPressed: () {
-                  setState(() {
-                    passenable = !passenable;
-                  });
-                },
-                icon: Icon(
-                  passenable ? Icons.visibility : Icons.visibility_off,
-                ),
-              ),
-            ),
-
-            SizedBox(height: 8),
-            WidgetButton(
-              namabutton: 'Tampil Data',
-              onPressed: () {
-                // MaterialPageRoute route = MaterialPageRoute(
-                //   builder: (context) => DashboardPage(),
-                // );
-                // Navigator.push(context, route);
-                setState(() {
-                  usernameController.text;
-                  paswordController.text;
-                });
-              },
-            ),
-
-            SizedBox(height: 8),
-            Text(
-              'Username: ${usernameController.text},\nPassword: ${paswordController.text}',
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
